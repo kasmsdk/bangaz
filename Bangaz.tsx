4f996f6c5987ab50c7815a7046ff49b67c81431a
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-17 14:33:27 +1000

    Kams SDK Baseline

--- Bangaz.tsx
+++ Bangaz.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const Bangaz: React.FC = () => {
-  return (
-    <div className="bangaz-main-ui" style={{ padding: '2em' }}>
-      <h1 style={{ fontSize: '2.5em', marginBottom: '0.5em' }}>Bangaz Drum Machine</h1>
-      <p style={{ color: '#7A6442', marginBottom: '2em' }}>
-        WebMIDI Drum Machine Instrument. <br />
-        <em>Drum pattern editor coming here soon...</em>
-      </p>
-    </div>
-  );
+    return (
+        <div className="bangaz-main-ui" data-testid="bangaz-root" style={{ padding: '2em' }}>
+            <h1 style={{ fontSize: '2.5em', marginBottom: '0.5em' }}>Bangaz MIDI Arpeggiator Editor</h1>
+            <p style={{ marginBottom: '2em' }}>
+                WebMIDI Bangaz Drum Pattern Browser and Editor Tool.<br />
+                <em>Mechanism to view drum sequences and edit them coming soon...</em>
+            </p>
+        </div>
+    );
 };
 
 export default Bangaz;
-
