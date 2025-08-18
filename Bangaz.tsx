4555851412f7b73eef226d4ec3b04fbaf6c4a063
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-18 23:58:58 +1000

    (chore) Basic doco structure

--- Bangaz.tsx
+++ Bangaz.tsx
@@ -4,11 +4,13 @@ import MidiSelector from '../components/MidiSelector';
 const Bangaz: React.FC = () => {
     return (
         <div style={{ padding: '2em' }}>
-            WebMIDI Bangaz Drum Pattern Browser and Editor Tool.<br />
+            <h1>Bangaz Drum Pattern Browser and Editor</h1>
             <p className="sunken-paragraph">
                 Bangaz are a collection step sequences meant to be played out General MIDI Drum kits and Ableton Drum Racks
                 Drum patterns are effectively emanators, where the current step is expected to map musically and consistently
                 over time</p>
+            <p className="sunken-paragraph">
+                Bangaz drum pattern browser/gallery and web based pattern edit tools are coming here soon...</p>
             <div style={{ margin: '20px 0' }}>
                 <div>
                     Connect to your MIDI device... (you might need a <a href="https://help.ableton.com/hc/en-us/articles/209774225-Setting-up-a-virtual-MIDI-bus" target="_blank">virtual MIDI bus</a>)
